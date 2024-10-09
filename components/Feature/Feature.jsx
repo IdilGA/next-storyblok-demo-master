59f90099eeabf9a7861cc4bd353556fba6661bfb
Idil <i364923@fhict.nl>
2024-10-09 08:43:51 +0200

    Update Feature

--- components/Feature/Feature.jsx
+++ components/Feature/Feature.jsx
@@ -2,7 +2,7 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import "./Feature.css" 
 const Feature = ({ blok }) => (
   <div className="blok-feature" {...storyblokEditable(blok)}>
-    <h2>Dit</h2>
+    <h2>Dit is waar text in moet</h2>
     {blok.name}
   </div>
 );
