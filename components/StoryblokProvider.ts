600fb2269ca390c880026db1c48a58a48cbc4501
Idil <i364923@fhict.nl>
2024-10-10 14:13:43 +0200

    testen

--- components/StoryblokProvider.ts
+++ components/StoryblokProvider.ts
@@ -1,5 +1,5 @@
 "use client";
-
+ 
 import {
   storyblokInit,
   apiPlugin,
@@ -7,20 +7,18 @@ import {
 } from "@storyblok/react/rsc";
 import Page from "./page.component";
 import Feature from "./Feature/Feature";
-import Header from "./header/Header";
-
+ 
 const components = {
   page: Page,
   feature: Feature,
-  header: Header,
 };
-
+ 
 storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
   components: components as SbReactComponentsMap,
 });
-
+ 
 export default function StoryblokProvider({
   children,
 }: {
@@ -28,5 +26,5 @@ export default function StoryblokProvider({
 }) {
   return children;
 }
-
+ 
 console.log(process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN);
--- components/page.component.tsx
+++ components/page.component.tsx
@@ -1,46 +1,24 @@
-import React from 'react';
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react/rsc';
-import Header from './header/Header';  // Importeer de Header component
-
-
+ 
 type Blok = {
   _uid: string;
   component: string;
-  [key: string]: unknown;
+  [key: string]: unknown; // If you have specific keys, it's better to define them here
 };
-
+ 
 type PageProps = {
   blok: {
     body: Blok[];
-    logo: {
-      filename: string;
-    };
   };
 };
-
-const Page: React.FC<PageProps> = ({ blok }) => {
-  // Haal het logo op van Storyblok, of gebruik een fallback als er geen logo is
- // const logoUrl = blok.logo?.filename || '/fallback-logo.png';  // Fallback logo als er geen asset is
-
-  // const navLinks = [
-  //   { label: 'Home', url: '/' },
-  //   { label: 'Nieuws', url: '/news' },
-  //   { label: 'Interviews', url: '/interviews' },
-  //   { label: 'Artikelen', url: '/articles' },
-  //   { label: 'Evenementen', url: '/events' },
-  // ];
-
-  return (
-    <main {...storyblokEditable(blok)}>
-      {/* Gebruik het logo dat van Storyblok komt */}
-      {/* <Header logo={logoUrl} links={navLinks} /> */}
-
-      {/* Render dynamisch alle blokken via Storyblok */}
-      {blok.body.map((nestedBlok) => (
-        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-      ))}
-    </main>
-  );
-};
-
+ 
+const Page: React.FC<PageProps> = ({ blok }) => (
+<main {...storyblokEditable(blok)}>
+    {blok.body.map((nestedBlok) => (
+      // eslint-disable-next-line
+<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+    ))}
+</main>
+);
+ 
 export default Page;
