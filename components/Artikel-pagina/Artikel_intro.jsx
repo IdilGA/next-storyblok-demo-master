18961cbc099c4294ebf847a83fc692795174047b
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-10-11 15:57:09 +0200

    intro gemaakt

--- components/Artikel-pagina/Artikel_intro.jsx
+++ components/Artikel-pagina/Artikel_intro.jsx
@@ -4,6 +4,12 @@ import "./artikel_intro.css";
 const artikel_intro = ({ blok }) => (
   <div className="Artikel_Intro" {...storyblokEditable(blok)}>
     <p className="Artikel_Titel">{blok.artikel_titel}</p>
+    <img
+                className="Artikel_lijn"
+                src={blok.artikel_lijn.filename}
+                alt={blok.artikel_lijn || "servicephoto"}
+    />
+    <p className="Artikel_Titel">{blok.artikel_inleiding}</p>
   </div>
 );
  
