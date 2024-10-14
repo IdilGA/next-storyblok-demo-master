5890ae5055f2c4318cb0635cc4b3cac9c98855a8
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-10-15 00:12:16 +0200

    Artikel-pagina bijna af

--- components/Artikel-pagina/Artikel_intro.jsx
+++ components/Artikel-pagina/Artikel_intro.jsx
@@ -5,7 +5,7 @@ import "./artikel_intro.css";
 
  
 const artikel_intro = ({ blok }) => (
-  <div className="Artikel_Intro" {...storyblokEditable(blok)}>
+  <div className="Artikel_Intro dark" {...storyblokEditable(blok)}>
     <p className="Artikel_Titel">{blok.artikel_titel}</p>
     <img
                 className="Artikel_lijn"
@@ -13,7 +13,7 @@ const artikel_intro = ({ blok }) => (
                 alt={blok.artikel_lijn || "servicephoto"}
     />
     <div className="inleiding-layout">
-    <p className="Artikel_inleiding">{blok.artikel_inleiding}</p>
+    <p className="Artikel_inleiding ">{blok.artikel_inleiding}</p>
     </div>
   </div>
 );
