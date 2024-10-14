d9ffcf3daea63190923fbee9af4fae8ca5b05ef9
Idil <i364923@fhict.nl>
2024-10-14 14:21:53 +0200

    testeb

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,97 +1,54 @@
-import React from "react";
-import "./Footer.css"; // Verwijst naar je eigen CSS-styling
+import { storyblokEditable, renderRichText } from "@storyblok/react/rsc";
+import "./Footer.css";
 
-const Footer = () => {
+const Footer = ({ blok }) => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        {/* TrendFocus Links Sectie */}
-        <div className="footer-section">
+        {/* TrendFocus Links Sectie met RichText */}
+        <div className="footer-section" {...storyblokEditable(blok)}>
           <h3 className="footer-heading">TrendFocus</h3>
-          <ul>
-            <li>
-              <a href="/about-us" className="footer-link">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="/privacy-policy" className="footer-link">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="/terms-of-service" className="footer-link">
-                Terms of Service
-              </a>
-            </li>
-          </ul>
+          <div className="footer-text">
+            {renderRichText(blok.trendfocus_links)}
+          </div>
         </div>
 
-        {/* Mijn TrendFocus Links Sectie */}
-        <div className="footer-section">
+        {/* My TrendFocus Links Sectie met RichText */}
+        <div className="footer-section" {...storyblokEditable(blok)}>
           <h3 className="footer-heading">My TrendFocus</h3>
-          <ul>
-            <li>
-              <a href="/login" className="footer-link">
-                Login
-              </a>
-            </li>
-            <li>
-              <a href="/my-subscription" className="footer-link">
-                My Subscription
-              </a>
-            </li>
-            <li>
-              <a href="/account-settings" className="footer-link">
-                Account Settings
-              </a>
-            </li>
-          </ul>
+          <div className="footer-text">
+            {renderRichText(blok.mytrendfocus_links)}
+          </div>
         </div>
 
-        {/* Contact Links Sectie */}
-        <div className="footer-section">
+        {/* Contact Links Sectie met RichText */}
+        <div className="footer-section" {...storyblokEditable(blok)}>
           <h3 className="footer-heading">Contact</h3>
-          <ul>
-            <li>
-              <a href="/customer-service" className="footer-link">
-                Customer Service
-              </a>
-            </li>
-            <li>
-              <a href="/editorial-team" className="footer-link">
-                Editorial Team
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="footer-link">
-                Contact Form
-              </a>
-            </li>
-          </ul>
+          <div className="footer-text">
+            {renderRichText(blok.contact_links)}
+          </div>
         </div>
 
         {/* Social Media Sectie */}
-        <div className="footer-section">
+        <div className="footer-section" {...storyblokEditable(blok)}>
           <h3 className="footer-heading">Follow Us</h3>
           <div className="social-icons">
-            <a href="https://facebook.com" className="social-icon">
-              <img src="/path-to-facebook-icon.png" alt="Facebook" />
-            </a>
-            <a href="https://twitter.com" className="social-icon">
-              <img src="/path-to-twitter-icon.png" alt="Twitter" />
-            </a>
-            <a href="https://linkedin.com" className="social-icon">
-              <img src="/path-to-linkedin-icon.png" alt="LinkedIn" />
-            </a>
-            <a href="https://instagram.com" className="social-icon">
-              <img src="/path-to-instagram-icon.png" alt="Instagram" />
-            </a>
+            {blok.social_media.map((social, index) => (
+              <a key={index} href={social.cached_url} className="social-icon">
+                {/* Dynamisch icoon vanuit Storyblok */}
+                {social.icon && (
+                  <img
+                    src={social.icon.filename}
+                    alt={social.icon.alt || "Social Media Icon"}
+                  />
+                )}
+              </a>
+            ))}
           </div>
         </div>
       </div>
 
-      {/* Hardcoded footer-bottom */}
+      {/* Footer-bottom hardcoded of dynamisch als nodig */}
       <div className="footer-bottom">
         <p>&copy; 2024 TrendFocus. All rights reserved.</p>
       </div>
