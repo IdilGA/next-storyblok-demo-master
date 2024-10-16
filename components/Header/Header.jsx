b1ace0e19a070291279a2a6f1a39832f0665a7e9
Idil <i364923@fhict.nl>
2024-10-16 16:33:39 +0200

    afrondingen navigatie bar aan het aanpassen

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,48 +2,90 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import "./Header.css";
 
 const Header = ({ blok }) => {
+  const newLocal = "bg-[#22252A] text-white py-2 mx-auto max-w-screen-xl w-full rounded-tl-lg rounded-br-lg";
   return (
-    <header className="header">
-      {/* Dynamisch logo van Storyblok */}
+    <header className="bg-white py-5 text-center">
+      {/* Logo sectie */}
       <div
-        className="logo-container flex justify-start"
+        className="container mx-auto flex justify-center items-center mb-5"
         {...storyblokEditable(blok)}
       >
-        <img src={blok.logo.filename} alt={blok.logo.alt} className="logo" />
+        <img src={blok.logo.filename} alt={blok.logo.alt} className="h-16" />
       </div>
-      <ul className="flex flex-row text-white gap-20">
-        <a href="home">
-          <li>Home</li>
+
+      {/* Registreren en Inloggen knoppen */}
+      <div className="flex justify-end space-x-4 mb-3 pr-10">
+        <a
+          href="/register"
+          className="px-4 py-1 bg-blue-400 text-white rounded-full"
+        >
+          Registreren
         </a>
-        <a href="/nieuws">
-          <li>Nieuws</li>
+        <a
+          href="/login"
+          className="px-4 py-1 bg-red-400 text-white rounded-full"
+        >
+          Inloggen
         </a>
-        <a href="interviews">
-          <li>Interviews</li>
-        </a>
-        <a href="artikelen">
-          <li>Artikelen</li>
-        </a>
-        <a href="evenementen-webinars">
-          <li>Evenementen & webinairs</li>
-        </a>
-      </ul>
+      </div>
 
       {/* Navigatiebalk */}
-      {/* <nav className="nav-bar">
-        <ul className="nav-list">
-          {links.map((link, index) => (
-            <li key={index} className="nav-item">
-              <a href={link.url} className="nav-link">
-                {link.label}
+      <nav className={newLocal}>
+        <div className="flex justify-between items-center">
+          <ul className="flex space-x-8">
+            <li>
+              <a href="/home" className="px-3 py-2 bg-blue-400 rounded-full">
+                Home
               </a>
             </li>
-          ))}
-        </ul>
-        <div className="search-bar">
-          <input type="text" placeholder="Zoeken..." className="search-input" />
+            <li>
+              <a href="/nieuws" className="hover:text-orange-300">
+                Nieuws
+              </a>
+            </li>
+            <li>
+              <a href="/interviews" className="hover:text-orange-300">
+                Interviews
+              </a>
+            </li>
+            <li>
+              <a href="/artikelen" className="hover:text-orange-300">
+                Artikelen
+              </a>
+            </li>
+            <li>
+              <a href="/evenementen-webinars" className="hover:text-orange-300">
+                Evenementen & Webinars
+              </a>
+            </li>
+          </ul>
+
+          {/* Zoekknop */}
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Zoeken..."
+              className="px-4 py-2 rounded-full text-black"
+            />
+            <button className="absolute right-2 top-2 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-4.35-4.35M10 18a8 8 0 100-16 8 8 0 000 16z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-      </nav> */}
+      </nav>
     </header>
   );
 };
