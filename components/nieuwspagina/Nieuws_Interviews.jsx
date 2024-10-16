5bdf55947aa40860d8e04314c2be48126e2fc45e
freekvermaas <freekvermaas@gmail.com>
2024-10-16 12:17:00 +0200

    Positionering / added artikelen
    alles staat in goede plek
    positionering gefixt
    lijn en meer toegevoegd

--- components/nieuwspagina/Nieuws_Interviews.jsx
+++ components/nieuwspagina/Nieuws_Interviews.jsx
@@ -3,7 +3,7 @@ import "./Nieuws_Interviews.css";
  
 const Nieuws_Interviews = ({ blok }) => (
   <div className="Nieuws_Interviews" {...storyblokEditable(blok)}>
-
+    <h1 className="Nieuws_Interviewtitel">{blok.Nieuws_Interviewtitel}</h1>
   </div>
 );
  
--- components/nieuwspagina/Nieuws_Lijn.jsx
+++ components/nieuwspagina/Nieuws_Lijn.jsx
@@ -3,7 +3,7 @@ import "./Nieuws_Lijn.css";
  
 const Nieuws_Lijn = ({ blok }) => (
   <div className="Nieuws_Lijn" {...storyblokEditable(blok)}>
-
+    <img className="Nieuws_Kleurlijn" src={blok.Nieuws_Kleurlijn.filename}/>
   </div>
 );
  
