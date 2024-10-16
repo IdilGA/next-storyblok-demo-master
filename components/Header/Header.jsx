e3fbdcf4ac54c30abb810fbfdb37dc37826d17dc
Idil <i364923@fhict.nl>
2024-10-16 17:21:30 +0200

    header aangepast
    afstand tussen links moeten nog en de twee knoppen boven moeten aangepast (tegen elkaar aan) worden en afgerond.

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,24 +1,17 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
-import "./Header.css";
+import "./Header.css"; // Vergeet niet je CSS-bestand te importeren
 
 const Header = ({ blok }) => {
-  const newLocal = "bg-[#22252A] text-white py-2 mx-auto max-w-screen-xl w-full rounded-tl-lg rounded-br-lg";
   return (
-    <header className="bg-white py-5 text-center">
-      {/* Logo sectie */}
-      <div
-        className="container mx-auto flex justify-center items-center mb-5"
-        {...storyblokEditable(blok)}
-      >
-        <img src={blok.logo.filename} alt={blok.logo.alt} className="h-16" />
+    <header className="header">
+      {/* Logo sectie met witte achtergrond */}
+      <div className="logo-container" {...storyblokEditable(blok)}>
+        <img src={blok.logo.filename} alt={blok.logo.alt} className="logo" />
       </div>
 
       {/* Registreren en Inloggen knoppen */}
-      <div className="flex justify-end space-x-4 mb-3 pr-10">
-        <a
-          href="/register"
-          className="px-4 py-1 bg-blue-400 text-white rounded-full"
-        >
+      <div className="auth-buttons">
+        <a href="/register" className="px-4 py-1 bg-blue-400 text-white -full">
           Registreren
         </a>
         <a
@@ -30,60 +23,41 @@ const Header = ({ blok }) => {
       </div>
 
       {/* Navigatiebalk */}
-      <nav className={newLocal}>
-        <div className="flex justify-between items-center">
-          <ul className="flex space-x-8">
-            <li>
-              <a href="/home" className="px-3 py-2 bg-blue-400 rounded-full">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/nieuws" className="hover:text-orange-300">
-                Nieuws
-              </a>
-            </li>
-            <li>
-              <a href="/interviews" className="hover:text-orange-300">
-                Interviews
-              </a>
-            </li>
-            <li>
-              <a href="/artikelen" className="hover:text-orange-300">
-                Artikelen
-              </a>
-            </li>
-            <li>
-              <a href="/evenementen-webinars" className="hover:text-orange-300">
-                Evenementen & Webinars
-              </a>
-            </li>
-          </ul>
+      <nav className="nav-bar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/home" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/nieuws" className="nav-link hover:text-orange-300">
+              Nieuws
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/interviews" className="nav-link hover:text-orange-300">
+              Interviews
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/artikelen" className="nav-link hover:text-orange-300">
+              Artikelen
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="/evenementen-webinars"
+              className="nav-link hover:text-orange-300"
+            >
+              Evenementen & Webinars
+            </a>
+          </li>
+        </ul>
 
-          {/* Zoekknop */}
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Zoeken..."
-              className="px-4 py-2 rounded-full text-black"
-            />
-            <button className="absolute right-2 top-2 text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-4.35-4.35M10 18a8 8 0 100-16 8 8 0 000 16z"
-                />
-              </svg>
-            </button>
-          </div>
+        {/* Zoekbalk */}
+        <div className="search-bar">
+          <input type="text" placeholder="Zoeken..." className="search-input" />
         </div>
       </nav>
     </header>
